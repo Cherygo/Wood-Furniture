33263406dd141ae04e9d65fadb736fd79933b90c
Cherygo <olexdzhy@gmail.com>
2026-02-12 22:56:15 +0200

    Add index method for handling filtering on the catalog page and inertia routing

--- app/Http/Controllers/FurnitureController.php
+++ app/Http/Controllers/FurnitureController.php
@@ -4,9 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class FurnitureController extends Controller
 {
+    public function index(Request $request)
+    {
+        $furniture = DB::table('furniture')
+            ->when($request->category, function ($q, $category) {
+                return $q->where('category', $category);
+            })
+            ->when($request->min_price, function ($q, $min_price) {
+                return $q->where('price', '>=', $min_price);
+            })
+            ->when($request->max_price, function ($q, $max_price) {
+                return $q->where('price', '<=', $max_price);
+            })
+            ->when($request->sort_price, function ($q, $sort_price) {
+               $direction = $sort_price === 'high_low' ? 'desc' : 'asc';
+               return $q->orderBy('price', $direction);
+            }, function($q){
+                return $q->latest();
+            })
+            ->paginate(16)
+            ->withQueryString();
+
+        $categories = DB::table('furniture')
+            ->select('category')
+            ->distinct()
+            ->pluck('category');
+
+        return Inertia::render('cat/index', [
+            'furniture' => $furniture,
+            'categories' => $categories,
+            'filters' => $request->only('category', 'min_price', 'max_price', 'sort_price'),
+        ]);
+    }
+
     public static function takeXLatestItems(int $amount) : object
     {
             $furniture = DB::table('furniture')
