38c3da341f1f8f8fb3f88fc58ea7e35d4a975d0e
Cherygo <olexdzhy@gmail.com>
2026-02-16 15:52:11 +0200

    Rename routes

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,9 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\FurnitureController as FurnitureController;
-use App\Models\Furniture;
+use App\Http\Controllers\FurnitureController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
-
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canRegister' => Features::enabled(Features::registration()),
-//     ]);
-// })->name('home');
-//
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
 Route::get('/', function () {
     return Inertia::render('index', array(
@@ -31,25 +18,26 @@ Route::group(['prefix' => 'auth'], function () {
     Route::get('/register', function () {
         return Inertia::render('auth/Register', []);
     })->middleware('guest')
-        ->name('register');
+        ->name('auth.register');
     Route::post('/register', RegisterController::class)
         ->middleware('guest');
 
     Route::get('/login', function () {
         return Inertia::render('auth/Login', []);
     })->middleware('guest')
-        ->name('login');
+        ->name('auth.login');
     Route::post('/login', LoginController::class)
         ->middleware('guest');
 
     Route::post('/logout', LogoutController::class)
         ->middleware('auth')
-        ->name('logout');
+        ->name('auth.logout');
 });
 
 // CATALOG
-Route::group(['prefix' => 'catalog'], function () {
-   Route::get('/', [FurnitureController::class, 'index'])->name('catalog.index');
-});
+   Route::get('/catalog', [FurnitureController::class, 'index'])->name('catalog.index');
+
+// PRODUCT PAGE
+Route::get('/product/{id}', [FurnitureController::class, 'show'])->name('product.show');
 
 require __DIR__.'/settings.php';
