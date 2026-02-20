637a39a9ad0ba09f9f8aae383c66ceab7c1553d3
Cherygo <olexdzhy@gmail.com>
2026-02-20 19:08:01 +0200

    Add very serious pages with company info

---
+++ resources/js/pages/company/About.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import MainLayout from '@/layouts/MainLayout.vue';
+</script>
+
+<template>
+    <MainLayout>
+        <Head title="About Us" />
+
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 text-center text-black text-4xl font-bold">
+            <h1 class="text-3xl font-bold text-gray-900 mb-4 text-center">Company yes wood a lot</h1>
+        </div>
+    </MainLayout>
+</template>
---
+++ resources/js/pages/company/Location.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import MainLayout from '@/layouts/MainLayout.vue';
+</script>
+
+<template>
+    <MainLayout>
+        <Head title="Where to Find Us" />
+
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 text-center text-black text-4xl font-bold">
+            <h1>Good luck finding it</h1>
+        </div>
+    </MainLayout>
+</template>
